ae778e31397209ce620b5e1762abeb6ffc57ce58
Brian Cain <bcain@hashicorp.com>
2023-03-14 14:37:36 -0700

    Add default color, add a version string

--- cmd/waypoint-station/main.go
+++ cmd/waypoint-station/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 
 	client "github.com/briancain/waypoint-station/waypoint-client"
+)
+
+const (
+	// TODO(briancain): Abstract this out to its own func so we can build
+	// release versions off of git SHAs rather than it being manual
+	Version = "0.1.0"
 )
 
 func main() {
@@ -11,6 +18,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	// Run application
+
+	// Default color
+	tview.Styles.PrimitiveBackgroundColor = tcell.NewRGBColor(40, 44, 48)
 
 	box := tview.NewBox().SetBorder(true).SetTitle("Hello, Waypoint!")
 	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
